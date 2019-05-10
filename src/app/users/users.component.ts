5519d97f93848611b3e0f4d839c46737e6dbc920
Mwaniki <mr.mwaniki@gmail.com>
2019-05-10 16:28:03 +0300

    update on users c ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,44 +9,42 @@ import { Users } from '../class/users';
 })
 export class UsersComponent implements OnInit {
 
-  user: Users;
+  users: Users;
   repos:any[];
-  users: Object;
   created_at:any;
   username:string;
 
+
   constructor(private dataService: DataService) {
-    this.user = new Users ("","","","","","","",0,false,new Date(),0,0);
-    // this.users = []
-
-    // this.dataService.getRepos().subscribe(repos => {
-    //   console.log(repos);
-    //   this.repos = repos;
-    // })
-
-    // Repeated subscribing of dataservice to allow for the display of default data 
-   }
-
-   
-
-  //  searchUser(){
-  //   this.dataService.updateUsers(this.username);
-  //   this.dataService.getUsers().subscribe(users => {
-  //    console.log(users);
-  //    this.users = users;
-  //  });
-
-  //  this.dataService.getRepos().subscribe(repos => {
-  //    console.log(repos);
-  //    this.repos = repos;
-  //  })
-  // }
-
-  ngOnInit() {
+    this.users = new Users ("","","","","","","",0,false,new Date(),0,0);
     this.dataService.getUsers().subscribe(users => {
       console.log(users);
       this.users = users;
     });
+
+    this.dataService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+
+    // Repeated subscribing of dataservice to allow for the display of default data 
+   }
+
+
+   searchUser(){
+     this.dataService.updateUsers(this.username);
+     this.dataService.getUsers().subscribe(users => {
+      console.log(users);
+      this.users = users;
+    });
+
+    this.dataService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+   }
+
+  ngOnInit() {
   }
 
 }
